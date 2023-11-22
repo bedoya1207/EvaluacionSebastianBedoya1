3b8bcae6eae4d373cfe23df890297b7cd5c617bb
bedoya1207 <69017454+bedoya1207@users.noreply.github.com>
2023-11-22 01:04:20 -0500

    Implementación de la clase Matriz con lectura y validación

--- src/main/main.java
+++ src/main/main.java
@@ -21,3 +21,41 @@ public class main {
         }
     }
 }
+class Matriz {
+    private int[][] matriz = new int[5][5];
+
+    public void leerSecuencia(Scanner scanner) {
+        try {
+            System.out.println("Ingrese un número y su ubicación en la matriz (fila, columna): ");
+            int numero = scanner.nextInt();
+            int fila = scanner.nextInt();
+            int columna = scanner.nextInt();
+
+            validarEntrada(fila, columna);
+
+            this.matriz[fila][columna] = numero;
+           
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Formato de entrada incorrecto.");
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    public void mostrarMatriz() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void validarEntrada(int fila, int columna) {
+        if (fila < 0 || fila >= 5 || columna < 0 || columna >= 5) {
+            throw new IllegalArgumentException("Ubicación fuera de los límites de la matriz.");
+        }
+    }
+}
+
+
